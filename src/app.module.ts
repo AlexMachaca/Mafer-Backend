bb5bfb6c558ebc96d7eec5c0f09f82a991acb78a
AlexMachaca <212129@unamba.edu.pe>
2024-07-23 00:29:05 -0500

    Ready to star, product and user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductModule } from './modulos/product/product.module';
+import { UserModule } from './modulos/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [ProductModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
