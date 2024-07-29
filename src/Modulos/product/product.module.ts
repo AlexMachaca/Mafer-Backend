f774efb7686cbc2a0bbbcd70b4d9cad95c7ec590
AlexMachaca <212129@unamba.edu.pe>
2024-07-29 17:24:41 -0500

    Los modulos de product, user

--- src/Modulos/product/product.module.ts
+++ src/Modulos/product/product.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './entity/ProductEntity.entity';
+import { CloudinaryService } from 'src/ServicesCloud/cloudinary/cloudinary.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Product])
+  ],
   controllers: [ProductController],
-  providers: [ProductService]
+  providers: [ProductService,CloudinaryService]
 })
 export class ProductModule {}
--- src/Modulos/user/user.module.ts
+++ src/Modulos/user/user.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entity/UserEntity.entity';
+import { ValidateService } from 'src/Common/Validate/validate.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User])
+  ],
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService,ValidateService]
 })
 export class UserModule {}
