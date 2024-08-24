961d3902f9ac8e4a4c023ceba6427b8f1c87363a
AlexMachaca <212129@unamba.edu.pe>
2024-08-24 01:31:36 -0500

    Modulo de Shipment

--- src/Modulos/shipment/shipment.module.ts
+++ src/Modulos/shipment/shipment.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ShipmentController } from './shipment.controller';
 import { ShipmentService } from './shipment.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Shipment } from './entity/ShipmentEntity.entity';
+import { User } from '../user/entity/UserEntity.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Shipment]),TypeOrmModule.forFeature([User])
+  ],
   controllers: [ShipmentController],
   providers: [ShipmentService]
 })
